13bf1d7cae967c8df5338fb8c9cea1f2d60fbf16
jessica.gamio@gmail.com <jessica.gamio@gmail.com>
2019-10-30 15:09:33 -0700

    creating tree and node class

--- break.py
+++ break.py
@@ -1,48 +1,67 @@
 
+class Node(object):
+    """Create Node"""
+    def __init__(self, data):
+        """Initializes instance"""
+        self.data = data
+        self.children = []
+
+class Tree(object):
+    """Creat Tree"""
+    def __init__(self):
+        self.root = None
+            
+
+
 
 def parse(phrase, vocab):
     """return a set of all possible phrases using vocabulary words"""
 
 
 
-    # Break the given phrase into vocab words
-
-        # iterate through phrase
-            # add char into a temp_word container 
-            # check to see if temp_word in vocabulary set
-            # if temp_sentence in vocabulary set
-                # Add a space to temp_sentence
-
-            # if temp word NOT in vocabulary
-                # continue adding char to temp_sentence 
-
-        # Add phrase into sentence set
 
 
 
-    # Run through phrase from right to left
+
+
+    # # Break the given phrase into vocab words
+
+    #     # iterate through phrase
+    #         # add char into a temp_word container 
+    #         # check to see if temp_word in vocabulary set
+    #         # if temp_sentence in vocabulary set
+    #             # Add a space to temp_sentence
+
+    #         # if temp word NOT in vocabulary
+    #             # continue adding char to temp_sentence 
+
+    #     # Add phrase into sentence set
+
+
+
+    # # Run through phrase from right to left
      
-    sentences=set()
-    temp_sentence = ""
-    temp_word = ""
+    # sentences=set()
+    # temp_sentence = ""
+    # temp_word = ""
 
-    for char in phrase:
-        temp_word+= char
+    # for char in phrase:
+    #     temp_word+= char
 
-        if temp_word in vocab:
-            temp_sentence += temp_word + " "
-            temp_word = ""       
+    #     if temp_word in vocab:
+    #         temp_sentence += temp_word + " "
+    #         temp_word = ""       
 
-    print('sentence ====>', temp_sentence)
+    # print('sentence ====>', temp_sentence)
 
-    sentence = temp_sentence
-    sentences.add(sentence)
-    # sentences.add(read_rightToLeft(sentence))
+    # sentence = temp_sentence
+    # sentences.add(sentence)
+    # # sentences.add(read_rightToLeft(sentence))
 
     
-    for sentence in sentences:
+    # for sentence in sentences:
 
-        print('set of sentences....',sentence)
+    #     print('set of sentences....',sentence)
 
 
 vocab = {'i', 'a', 'ten', 'oodles', 'ford', 'inner', 'to', 'night', 'ate', 'noodles', 'for', 'dinner', 'tonight'}
